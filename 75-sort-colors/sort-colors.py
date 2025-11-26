8c3e3312153721d847c86b9ef37d1f01b5b15ec9
Leul Tesfaye <leultesfaye0755@gmail.com>
2025-11-26 11:08:52 -0800

    Time: 0 ms (100.00%) | Memory: 12.4 MB (82.10%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        low =0
+        mid=0
+        high = len(nums)-1
+
+        while mid<= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid]= nums[mid], nums[low]
+                low +=1
+                mid +=1
+
+            elif nums[mid]==1:
+                mid +=1
+
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -=1
+
+        return nums
