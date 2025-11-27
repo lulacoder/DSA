31af03ce7562c8a918409f2a4d4664ac36207ef0
Leul Tesfaye <leultesfaye0755@gmail.com>
2025-11-27 02:57:00 -0800

    Time: 91 ms (18.64%) | Memory: 28.3 MB (96.33%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -5,13 +5,26 @@ class Solution(object):
         :type pivot: int
         :rtype: List[int]
         """
-        result =[]
-        greater =[num for num in nums if num > pivot]
-        less = [num for num in nums if num < pivot]
-        pivot1= [num for num in nums if num == pivot ]
-
-        result.extend(less)
-        result.extend(pivot1)
-        result.extend(greater)
+        n = len(nums)
+        ans = [0] * n
+        left = 0
+        right = n - 1
         
-        return result
+        # 'xrange' is used in Python 2 for memory efficient iteration (returns generator)
+        for i in xrange(n):
+            # Fill smaller elements from the start
+            if nums[i] < pivot:
+                ans[left] = nums[i]
+                left += 1
+                
+            # Fill larger elements from the end (reverse iteration)
+            if nums[n - 1 - i] > pivot:
+                ans[right] = nums[n - 1 - i]
+                right -= 1
+        
+        # Fill the remaining middle gap with the pivot value
+        while left <= right:
+            ans[left] = pivot
+            left += 1
+            
+        return ans
