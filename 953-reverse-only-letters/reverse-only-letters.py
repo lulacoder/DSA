f41da41b42ca86dd2df02a74a7af94f795ac0fc5
Leul Tesfaye <leultesfaye0755@gmail.com>
2025-12-02 11:48:20 -0800

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.35%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.py
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # Convert to list for mutability
         s_list = list(s)
-        left, right =0, len(s_list)-1
-
-        while left< right:
-            if not s_list[left].isalpha():
-                left +=1
-            elif not s_list[right].isalpha():
-                right-=1
-            else:
-                s_list[left], s_list[right]= s_list[right], s_list[left]
-                left+=1
-                right -=1
-
-        return "".join(s_list)
+        left, right = 0, len(s_list) - 1
         
+        # Two pointer approach
+        while left < right:
+            # Find letter from left
+            while left < right and not s_list[left].isalpha():
+                left += 1
+            # Find letter from right
+            while left < right and not s_list[right].isalpha():
+                right -= 1
+            # Swap the letters
+            s_list[left], s_list[right] = s_list[right], s_list[left]
+            left += 1
+            right -= 1
+        
+        return ''.join(s_list)
